257e80158e87cc26a51f62620b5bfa30c2af24af
Antonio Murcia <amurciaa61@gmail.com>
2020-11-01 20:42:00 +0100

    Agregar archivos de proyecto.

---
+++ CalculadoraDinamica/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculadoraDinamica
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string textoMostrar;
+        public MainWindow()
+        {
+            InitializeComponent();
+            textoMostrar = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string valor = (sender as Button).Tag.ToString();
+            textoMostrar += valor;
+            valorTextBlock.Text = textoMostrar;
+        }
+    }
+}
